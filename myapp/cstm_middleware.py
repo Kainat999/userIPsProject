0d5ca9c1eb0368435a815dfff1eddcc17ea16ac9
kainat999 <kainatbajwah999@gmail.com>
2023-09-15 20:22:00 +0500

    full_project

--- myapp/cstm_middleware.py
+++ myapp/cstm_middleware.py
@@ -1,19 +1,17 @@
-# middleware.py
 import logging
 import time
-
 logger = logging.getLogger(__name__)
 
-class IPLoggingMiddleware:
+class IP_Logging_Middleware:
     def __init__(self, get_response):
         self.get_response = get_response
         self.blocked_ips = {}
-        self.blocked_duration = 60  # 1 minute
+        self.blocked_duration = 60  # 60 sec = 1 minut
 
     def __call__(self, request):
         user_ip = self.get_client_ip(request)
         logger.info(f"User IP: {user_ip}, Time: {time.strftime('%Y-%m-%d %H:%M:%S')}")
-
+               # conditions
         if request.user.is_authenticated:
             group = request.user.groups.first()
             if group:
@@ -24,7 +22,7 @@ class IPLoggingMiddleware:
                     'Bronze': 5,
                 }.get(group_name, 1)
                 if self.is_blocked(user_ip, max_requests):
-                    raise Exception("User blocked due to excessive requests. Try again later.")
+                    raise Exception("User blocked. Try again later.")
 
         response = self.get_response(request)
         return response  
--- userIPsProject/settings.py
+++ userIPsProject/settings.py
@@ -48,7 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'myapp.cstm_middleware.IPLoggingMiddleware',
+    'myapp.cstm_middleware.IP_Logging_Middleware',
     
 
 ]
